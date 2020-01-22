fa0ae3f3edf1e7ae0b18b549ea41bfbe862ec1d4
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-22 23:19:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -61,7 +61,7 @@ def runner
 
   until card_count >= 21 do
     new = hit?(card_count)
-    display_card_total(card_count)
+    puts display_card_total(card_count)
     card_count = new
   end
   end_game(card_count)
