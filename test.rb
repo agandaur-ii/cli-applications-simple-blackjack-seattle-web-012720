3c45da6e88a3ded4e35aa7c33fe0481409e907b1
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-22 22:51:31 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -39,11 +39,11 @@ def hit?(card_total)
   end
   
   if input == "s"
-    return card_total
+    puts card_total
   end
   
   if input == "h"
-    return deal_card + card_total
+    puts deal_card + card_total
   end
   puts "=" * 20
   get_user_input
